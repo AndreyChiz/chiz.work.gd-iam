219bf4b33ca770b41f7da4d1accbc350867775a1
Andrey <Achizhovjob@yandex.ru>
2025-07-07 22:38:50 +0400

    ♻️ chore(clean): main.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,17 @@
 from contextlib import asynccontextmanager
-from typing import Annotated
 
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy import text
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import FastAPI
 
 from app.database import db_master
 
-app = FastAPI()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
     await db_master.dispose()
 
+app = FastAPI()
 
 @app.get("/")
 async def root():
     return {str(db_master.engine.url)}
-
-
-
--- dev_notes/command_log.md
+++ dev_notes/command_log.md
@@ -25,7 +25,7 @@ uv run pytest -v
 ## Alembic
 
 ```sh
- uv add --dev alembic   
+uv add --dev alembic   
 alembic init -t async alembic
 alembic revision --autogenerate -m "Initial"
 alembic upgrade head 
