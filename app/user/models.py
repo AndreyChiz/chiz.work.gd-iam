5bbea046fbc2ddefe557c44953eecea356be4d29
Andrey <Achizhovjob@yandex.ru>
2025-07-08 23:27:48 +0400

    📚 docs(newermind): TODOES

--- app/user/models.py
+++ app/user/models.py
@@ -5,16 +5,16 @@ from app.database import Base
 
 
 class User(Base):
-
     __include_id__ = True
     __id_type__ = "int"
     __include_created_at__ = True
     __include_updated_at__ = True
 
-    username: Mapped[str] 
+    username: Mapped[str]
     password: Mapped[str] = mapped_column(LargeBinary)
     role: Mapped[str]
-    frmware_acces_groups: Mapped[str]
+    frmware_acces_groups: Mapped[str]  # TODO add comments to fields
+    # TODO Check nullable marks
 
     is_active: Mapped[bool] = mapped_column(
         default=True,
