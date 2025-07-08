dfd23a356c9301703c8d85a9e90b0812df21a5ff
Andrey <Achizhovjob@yandex.ru>
2025-07-08 21:56:16 +0400

    ✨ feat(user): add get_users api endpoint

--- app/user/router.py
+++ app/user/router.py
@@ -1,3 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import db_master
+
+from .crud import get_all_users
+from .schema import RspUserSchema
+
 
 router = APIRouter()
+
+
+@router.get("", response_model=list[RspUserSchema])
+async def get_users(
+    session: AsyncSession = Depends(
+        db_master.session_getter,
+    ),
+):
+    users = await get_all_users(session=session)
+    return users
