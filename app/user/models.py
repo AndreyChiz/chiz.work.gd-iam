b6f69fe847a9ffca7bc3f6cc573a65b9ecd5230e
Andrey <Achizhovjob@yandex.ru>
2025-07-08 21:44:06 +0400

    📦 refactor(user): update User model

--- app/user/models.py
+++ app/user/models.py
@@ -9,6 +9,8 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] 
     password: Mapped[str] = mapped_column(LargeBinary)
+    role: Mapped[str]
+    frmware_acces_group: Mapped[str]
     created_at: Mapped[DateTime] = mapped_column(
         server_default=func.CURRENT_TIMESTAMP(),
         type_=DateTime(timezone=True),
