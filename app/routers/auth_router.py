8c1acc9126ea4dbb51dc1143c1519c36e6794c70
Andrey Chizhov <Achizhovjob@yandex.ru>
2025-07-16 22:01:31 +0400

    📦 refactor(auth): add check passvord validate and is active

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,29 +1,36 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends 
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import db_master
-from app.services.auth import LoginForm, UnautorisedException, TokenResponse
+from app.services.auth import (
+    LoginForm,
+    UnautorisedException,
+    # TokenResponse,
+    auth_service,
+)
 from app.services.user import user_crud
 
 
 router = APIRouter()
 
 
-
-
-@router.post("/login", response_model=TokenResponse)
+@router.post(
+    "/login",
+    # response_model=TokenResponse,
+)
 async def login(
     session: Annotated[AsyncSession, Depends(db_master.session_getter)],
-    form: LoginForm = Depends()
-
+    form: LoginForm = Depends(),
 ):
-    
     username = form.username
     password = form.password
-    if not (user:= await user_crud.get(session, username)):
+    if not (
+        (user := await user_crud.get(session, username))
+        and (auth_service.password.validate_password(password, user.password_hash))
+        and (user.is_active)
+    ):
         raise UnautorisedException
-    
 
     return user
