6b415007fc5bc0bb3ab1ed84d9a391afb1a60f37
Andrey Chizhov <Achizhovjob@yandex.ru>
2025-07-16 21:33:18 +0400

    refactor(auth): add check is user exist

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,20 +1,29 @@
-from fastapi import APIRouter
 from typing import Annotated
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.services.user import user_crud
-from app.services.auth import auth_manager
-from fastapi import APIRouter, Depends, status
-from app.database import db_master
-from app.services.user import InCreateUserSchema, OutUserSchema
 
-from app.services.auth import InLoginSchema
+from fastapi import APIRouter, Depends 
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import db_master
+from app.services.auth import LoginForm, UnautorisedException, TokenResponse
+from app.services.user import user_crud
+
 
 router = APIRouter()
 
 
-@router.post("/login", response_model=OutUserSchema)
+
+
+@router.post("/login", response_model=TokenResponse)
 async def login(
     session: Annotated[AsyncSession, Depends(db_master.session_getter)],
-    username: InLoginSchema,
+    form: LoginForm = Depends()
+
 ):
-    return await user_crud.get(session, username.username)
+    
+    username = form.username
+    password = form.password
+    if not (user:= await user_crud.get(session, username)):
+        raise UnautorisedException
+    
+
+    return user
